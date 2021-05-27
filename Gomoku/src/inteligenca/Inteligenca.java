6cce1710498423fe040ae8f78bb05fd3a50468d8
Nik Mrhar <you@example.com>
2021-05-27 18:24:07 +0200

    dokoncana inteligenca

--- Gomoku/src/inteligenca/Inteligenca.java
+++ Gomoku/src/inteligenca/Inteligenca.java
@@ -1,5 +1,6 @@
 package inteligenca;
 
+
 import java.util.List;
 import logika.Igra;
 import logika.Igralec;
@@ -9,7 +10,8 @@ import splosno.KdoIgra;
 
 public class Inteligenca extends KdoIgra {
 	
-	private static final int ZMAGA = 1000000000; // vrednost zmage
+	public static int evaluationCount = 0;
+	private static final int ZMAGA = 1000000; // vrednost zmage
 	private static final int ZGUBA = -ZMAGA;  // vrednost izgube
 	private static final int NEODLOC = 0;  // vrednost neodločene igre	
 	
@@ -22,22 +24,32 @@ public class Inteligenca extends KdoIgra {
 	
 	
 	public Koordinati izberiPotezo (Igra igra) {
+		
+		long startTime = System.currentTimeMillis();
+		
 		Koordinati r = poisciZmago(igra);
 		if (r != null) return r;
 		
 		Koordinati q = poisciPoraz(igra);
 		if (q != null) return q;
 		
-		return alphabetaPoteze(igra, this.globina, ZGUBA, ZMAGA, igra.naPotezi, 0).poteza;
+		Koordinati poteza = alphabeta(igra, this.globina, ZGUBA, ZMAGA, igra.naPotezi, 0).poteza;
+		
+		
+		
+		System.out.println("Cases calculated: " + evaluationCount + " Calculation time: " + (System.currentTimeMillis() - startTime) + " ms");
+		
+		return poteza;
 	}
+			
 	
-	
-	public static OcenjenaPoteza alphabetaPoteze(Igra igra, int globina, int alpha, int beta, Igralec jaz, int zavlacuj) {
+	public static OcenjenaPoteza alphabeta(Igra igra, int globina, int alpha, int beta, Igralec jaz, int zavlacuj) {
 		int ocena;
 		// Če sem računalnik, maksimiramo oceno z začetno oceno ZGUBA
 		// Če sem pa človek, minimiziramo oceno z začetno oceno ZMAGA
 		if (igra.naPotezi == jaz) {ocena = ZGUBA;} else {ocena = ZMAGA;}
 		List<Koordinati> moznePoteze = igra.poteze_omejeno();
+//		System.out.println(moznePoteze.size());
 		Koordinati kandidat = moznePoteze.get(0); // Možno je, da se ne spremini vrednost kanditata. Zato ne more biti null.
 			
 		for (Koordinati p: moznePoteze) {
@@ -51,7 +63,7 @@ public class Inteligenca extends KdoIgra {
 			default:
 				// Nekdo je na potezi
 				if (globina == 1) ocenap = OceniPozicijo.oceniPozicijo(kopijaIgre, jaz);
-				else ocenap = alphabetaPoteze (kopijaIgre, globina-1, alpha, beta, jaz, zavlacuj + 1).ocena;
+				else ocenap = alphabeta (kopijaIgre, globina-1, alpha, beta, jaz, zavlacuj + 1).ocena;
 			}
 			if (igra.naPotezi == jaz) { // Maksimiramo oceno
 				if (ocenap > ocena) { // mora biti > namesto >=
@@ -89,6 +101,5 @@ public class Inteligenca extends KdoIgra {
 		Koordinati p = poisciZmago(kopijaIgre);
 		return p;
 	}
-	
 }
 
--- Gomoku/src/inteligenca/OceniPozicijo.java
+++ Gomoku/src/inteligenca/OceniPozicijo.java
@@ -7,10 +7,9 @@ import logika.Vrsta;
 
 public class OceniPozicijo {
 	
-	// Metoda oceniPozicijo za igro TicTacToe
-	
 	
 	public static int oceniPozicijo(Igra igra, Igralec jaz) {
+		Inteligenca.evaluationCount++;
 		int ocena = 0;
 		for (Vrsta v : Igra.VRSTE) {
 			ocena = ocena + oceniVrsto(v, igra, jaz);
@@ -20,33 +19,34 @@ public class OceniPozicijo {
 	
 	public static int oceniVrsto (Vrsta v, Igra igra, Igralec jaz) {
 		Polje[][] plosca = igra.getPlosca();
+		boolean zaporedoma = true;
+		int zaporedno = 0;
 		int count_B = 0;
 		int count_W = 0;
 		for (int k = 0; k < 5 && (count_B == 0 || count_W == 0); k++) {
 			switch (plosca[v.x[k]][v.y[k]]) {
-			case B: count_B += 1; break;
-			case W: count_W += 1; break;
-			case PRAZNO: break;
+			case B: {count_B += 1; zaporedno += 1;} break;
+			case W: {count_W += 1; zaporedno += 1;} break;
+			case PRAZNO: {if (zaporedno > 0 && zaporedno < 4) {zaporedoma = !zaporedoma; zaporedno = 0;}} break;
 			}
 		}
 		if (count_B > 0 && count_W > 0) { return 0; }
 		return oceni(count_B,count_W,jaz);
 	}
 	
-	
 	public static int oceni(int count_B, int count_W, Igralec jaz) {
 		int score = 0;
 		if (jaz == Igralec.B) {
 			switch (count_B - count_W) {
 			case 5: score = 1000000;break;
-			case 4: score = 10000; break;
-			case 3: score = 50;break;
-			case 2: score = 20;break;
+			case 4: score = 5000; break;
+			case 3: score = 2000;break;
+			case 2: score = 500;break;
 			case 1: score = 5;break;
 			case -1: score = -5;break;
-			case -2: score = -20;break;
-			case -3: score = -50;break;
-			case -4: score = -10000;break;
+			case -2: score = -1500;break;
+			case -3: score = -6000;break;
+			case -4: score = -15000;break;
 			case -5: score = -1000000;break;
 			}
 			
@@ -54,106 +54,115 @@ public class OceniPozicijo {
 		else {
 			switch (count_W - count_B) {
 			case 5: score = 1000000;break;
-			case 4: score = 10000;break;
-			case 3: score = 50;break;
-			case 2: score = 20;break;
+			case 4: score = 5000; break;
+			case 3: score = 2000;break;
+			case 2: score = 500;break;
 			case 1: score = 5;break;
 			case -1: score = -5;break;
-			case -2: score = -20;break;
-			case -3: score = -50;break;
-			case -4: score = -10000;break;
+			case -2: score = -1500;break;
+			case -3: score = -6000;break;
+			case -4: score = -15000;break;
 			case -5: score = -1000000;break;
 			}
 		}
 		return score;
 	}
+	
+	
+//	public static int oceni1(int count_B, int count_W, Igralec jaz, boolean zaporedoma) {
+//		int score = 0;
+//		if (jaz == Igralec.B && zaporedoma == true) {
+//			switch (count_B - count_W) {
+//			case 5: score = 1000000;break;
+//			case 4: score = 5000; break;
+//			case 3: score = 2000;break;
+//			case 2: score = 500;break;
+//			case 1: score = 5;break;
+//			case -1: score = -5;break;
+//			case -2: score = -1200;break;
+//			case -3: score = -5000;break;
+//			case -4: score = -12000;break;
+//			case -5: score = -1000000;break;
+//
+//			}
+//			
+//		}
+//		else if (jaz == Igralec.W && zaporedoma == true) {
+//			switch (count_W - count_B) {
+//			case 5: score = 1000000;break;
+//			case 4: score = 5000; break;
+//			case 3: score = 2000;break;
+//			case 2: score = 500;break;
+//			case 1: score = 5;break;
+//			case -1: score = -5;break;
+//			case -2: score = -1200;break;
+//			case -3: score = -5000;break;
+//			case -4: score = -12000;break;
+//			case -5: score = -1000000;break;
+//			}
+//		}
+//		else if (jaz == Igralec.B) {
+//			switch (count_B - count_W) {
+//			case 5: score = 1000000;break;
+//			case 4: score = 4000; break;
+//			case 3: score = 1500;break;
+//			case 2: score = 300;break;
+//			case 1: score = 5;break;
+//			case -1: score = -5;break;
+//			case -2: score = -700;break;
+//			case -3: score = -3700;break;
+//			case -4: score = -10000;break;
+//			case -5: score = -1000000;break;
+//			}
+//		}
+//		else {
+//			switch (count_W - count_B) {
+//			case 5: score = 1000000;break;
+//			case 4: score = 4000; break;
+//			case 3: score = 1500;break;
+//			case 2: score = 300;break;
+//			case 1: score = 5;break;
+//			case -1: score = -5;break;
+//			case -2: score = -700;break;
+//			case -3: score = -3700;break;
+//			case -4: score = -10000;break;
+//			case -5: score = -1000000;break;
+//			}
+//		}
+//		return score;
+//	}
+//	
+//	public static int oceni2(int count_B, int count_W, Igralec jaz) {
+//		int score = 0;
+//		if (jaz == Igralec.B) {
+//			switch (count_B - count_W) {
+//			case 5: score = 1000000;break;
+//			case 4: score = 10000; break;
+//			case 3: score = 3000;break;
+//			case 2: score = 500;break;
+//			case 1: score = 5;break;
+//			case -1: score = -5;break;
+//			case -2: score = -1250;break;
+//			case -3: score = -7500;break;
+//			case -4: score = -30000;break;
+//			case -5: score = -1000000;break;
+//			}
+//			
+//		}
+//		else {
+//			switch (count_W - count_B) {
+//			case 5: score = 1000000;break;
+//			case 4: score = 10000; break;
+//			case 3: score = 3000;break;
+//			case 2: score = 500;break;
+//			case 1: score = 5;break;
+//			case -1: score = -5;break;
+//			case -2: score = -1250;break;
+//			case -3: score = -7500;break;
+//			case -4: score = -30000;break;
+//			case -5: score = -1000000;break;
+//			}
+//		}
+//		return score;
+//	}
 }	
-		
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
