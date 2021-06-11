46a1e9702fca83d65352bcf5a6fc02b6dbdb69ab
MrharN18 <49908464+MrharN18@users.noreply.github.com>
2021-06-11 11:12:23 +0200

    dodani komentarji kode

--- Gomoku/src/inteligenca/OcenjenaPoteza.java
+++ Gomoku/src/inteligenca/OcenjenaPoteza.java
@@ -2,6 +2,7 @@ package inteligenca;
 
 import splosno.Koordinati;
 
+// razred, ki predstavi potezo skupaj z njeno oceno
 public class OcenjenaPoteza {
 	
 	Koordinati poteza;
@@ -12,6 +13,7 @@ public class OcenjenaPoteza {
 		this.ocena = ocena;
 	}
 	
+	// primerjava dveh ocenjenih potez
 	public int compareTo (OcenjenaPoteza op) {
 		if (this.ocena < op.ocena) return -1;
 		else if (this.ocena > op.ocena) return 1;
