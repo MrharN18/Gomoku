87a86106d2a856f3b780d7e40d81c62403dd138d
MrharN18 <49908464+MrharN18@users.noreply.github.com>
2021-06-11 10:28:27 +0200

    dodani komentarji kode

--- Gomoku/src/inteligenca/NajboljseOcenjenePoteze.java
+++ Gomoku/src/inteligenca/NajboljseOcenjenePoteze.java
@@ -6,16 +6,19 @@ import inteligenca.OcenjenaPoteza;
 
 public class NajboljseOcenjenePoteze {
 	
+	// deklariramo seznam ocenjenih potez v katerega bomo shranjevali najboljse poteze
 	private LinkedList<OcenjenaPoteza> buffer;
-		
+	
+	// metoda, ki inicializira vrednost seznama
 	public NajboljseOcenjenePoteze() {
 		this.buffer = new LinkedList<OcenjenaPoteza> ();
 	}
 	
+	// ce je poteza boljsa od vseh potez v seznamu jo dodamo in ostale poteze izbrisemo iz seznama, ce je poteza slabsa je v seznam ne dodamo
 	public void addIfBest(OcenjenaPoteza ocenjenaPoteza) {
 		if (buffer.isEmpty()) buffer.add(ocenjenaPoteza);
 		else {
-			OcenjenaPoteza op = buffer.getFirst();
+			OcenjenaPoteza op = buffer.getFirst(); // prvo potezo v seznamu uporabimo za primerjavo
 			switch (ocenjenaPoteza.compareTo(op)) {
 			case 1: 
 				buffer.clear();  // ocenjenaPoteza > op
@@ -25,7 +28,7 @@ public class NajboljseOcenjenePoteze {
 		}
 	}
 	
-	
+	// iz LinkedList naredimo List
 	public List<OcenjenaPoteza> list() {
 		return (List<OcenjenaPoteza>) buffer;
 	}
