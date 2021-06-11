ca070ccf4ff41a559007089733477b0ee16ad0ae
MrharN18 <49908464+MrharN18@users.noreply.github.com>
2021-06-11 10:39:33 +0200

    dodani komentarji kode

--- Gomoku/src/vodja/VrstaIgralca.java
+++ Gomoku/src/vodja/VrstaIgralca.java
@@ -1,8 +1,10 @@
 package vodja;
 
+// enum razred doloca mozne vrste igralca
 public enum VrstaIgralca {
 	R, C; 
 
+	// to metodo uporabi print, ce mora izpisati element razreda
 	@Override
 	public String toString() {
 		switch (this) {
