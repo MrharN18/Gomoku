fdd00bb0dde7275d658180248f2e355bf1345c99
MrharN18 <49908464+MrharN18@users.noreply.github.com>
2021-06-11 10:35:40 +0200

    dodani komentarji kode

--- Gomoku/src/Gomoku.java
+++ Gomoku/src/Gomoku.java
@@ -4,12 +4,13 @@ import vodja.Vodja;
 import gui.Okno;
 
 public class Gomoku {
-
+	
+	// metoda main ustvari novo instanco razreda Okno
 	public static void main(String[] args) throws IOException {
 		Okno okno = new Okno(15);
 		okno.pack();
 		okno.setVisible(true);
-		Vodja.okno = okno;
+		Vodja.okno = okno; 
 	}
 
 }
